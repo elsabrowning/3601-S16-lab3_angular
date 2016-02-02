5f56fc3e0e6413e89dbb6b8324546750487036c5
brow3924 <brow3924@morris.umn.edu>
2016-02-02 13:37:44 -0600

    Added footer files

---
+++ public/javascript/footer.controller.js
@@ -0,0 +1,15 @@
+/**
+ * Created by brow3924 on 2/2/16.
+ */
+//====================== FOOTER CONTROLLER ============================
+angular.module('stdComponents').controller('footerCtrl', function(){
+    var footer = this;
+    console.log("footer controller loaded");
+
+    footer.textField = "STUFF";
+
+    footer.pages = [
+        {text: "Home", link: '/'},
+        {text: "About", link: '/about'},
+    ];
+});
